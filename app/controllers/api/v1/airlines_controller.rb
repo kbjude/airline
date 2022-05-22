510182bb783dbe4f1b83c5aaab5fb085f167b460
Jude <jude@Judes-MacBook-Pro.local>
2022-05-22 13:46:30 +0300

    updates on the api

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -7,7 +7,28 @@ module Api
               render json: AirlineSerializer.new(airlines).serialized_json
             end
             def show
-                airline Airline.
+                airline = Airline.find_by(slug:params[:slug])
+
+                render json: AirlineSerializer.new(airline).serialized_json
+            end
+            def create
+                airline = Airline.new(airline_params)
+
+                if airline.save
+                    render json: AirlineSerializer.(airline).serialized_json
+                else
+                    render json: { error: airline.errors.messages}, status: 422
+                end
+            end
+
+            def update
+                airline = Airline.new(airline_params)
+
+                if airline.save
+                    render json: AirlineSerializer.new(airline).serialized_json
+                else
+                    render json: {error: airline.errors.messages}, status: 422
+                end
             end
         end
     end
