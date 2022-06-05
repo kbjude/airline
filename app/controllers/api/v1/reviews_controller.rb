f0406045ea2d2f46f7cabc10e12b9e12d77c994e
kbjude <kbjude@gmail.com>
2022-06-05 05:14:17 +0300

    Going for testing

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,12 +1,20 @@
 module API
     module V1
-        class AirlinesController < ApplicationController
+        class ReviewsController < ApplicationController
             def create
                 review = Review.new(review_params)
                 if review.save
                     render json: ReviewSerializer.new(review).serialized_json
                 else
-                    render json: { error: review.errors.messages}
+                    render json: { error: review.errors.messages}, status: 422
+                end
+            end
+            def destroy
+                review = Review.find_by(params[:id])
+                if review.destroy
+                    head :no_content
+                else
+                    render json: { error: review.errors.messages}, status: 422
                 end
             end
 
