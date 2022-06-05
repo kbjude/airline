fff666e3f2a4d2b7fe7367a2ae0472d8715fd15e
Jude <jude@Judes-MacBook-Pro.local>
2022-06-05 04:25:06 +0300

    WIP airlines controller

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -4,12 +4,12 @@ module Api
             def index
               airlines = Airline.all
               
-              render json: AirlineSerializer.new(airlines).serialized_json
+              render json: AirlineSerializer.new(airlines, options).serialized_json
             end
             def show
-                airline = Airline.find_by(slug:params[:slug])
+                airline = Airline.find_by(slug: params[:slug])
 
-                render json: AirlineSerializer.new(airline).serialized_json
+                render json: AirlineSerializer.new(airline, options).serialized_json
             end
             def create
                 airline = Airline.new(airline_params)
@@ -22,13 +22,32 @@ module Api
             end
 
             def update
-                airline = Airline.new(airline_params)
+                airline = Airline.find_by(slug: param[:slug])
 
-                if airline.save
+                if airline.update(airline_params)
+                    render json: AirlineSerializer.new(airline, options).serialized_json
+                else
+                    render json: {error: airline.errors.messages}, status: 422
+                end
+            end
+            def destroy
+                airline = Airline.find_by(slug: param[:slug])
+
+                if airline.destroy(airline_params)
                     render json: AirlineSerializer.new(airline).serialized_json
                 else
                     render json: {error: airline.errors.messages}, status: 422
                 end
+            end
+
+            private
+
+            def airline_params
+                params.require(:airline).permit(:name, :name_url)
+            end
+
+            def options
+                @options ||= { include %i[reviews] }
             end
         end
     end
